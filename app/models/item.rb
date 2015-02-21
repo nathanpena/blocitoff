792d3e8785c8d6f1a47bb67e85a64e84be099590
nathanpena <nnpena@outlook.com>
2015-02-21 13:46:10 -0500

    tidy up html and days_left method

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ActiveRecord::Base
   belongs_to :list
 
   def days_left
-   ((created_at.to_date + 6.days) - DateTime.now.to_date).to_i
+   ((created_at.to_date + 7.days) - DateTime.now.to_date).to_i
   end
 end
