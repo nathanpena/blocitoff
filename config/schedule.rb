20004eaae9cd73a2bcf7ddae8dac5d53c906d6bb
nathanpena <nnpena@outlook.com>
2015-02-22 01:02:07 -0500

    fixes

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-every 1.day, :at => '12:20 am'  do  
+every 1.day, :at => '1:00 am'  do  
   rake 'todo:delete_items'  
   command 'cd /Users/nathanpena/code/blocitoff && RAILS_ENV=development bundle exec rake todo:delete_items'
 end
